b58a0a7f990b9832543a4434c357cc4fd41d8c52
sehgalansh <31631075+sehgalansh@users.noreply.github.com>
2018-09-04 09:08:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,15 @@
-
-//Kudo's discord quicktask bot v2.0
-
-
-
-
-
-
-var Discord = require('discord.io');
+//Kudo's discord quicktask bot v1.0
+//mimics keyboard shortcuts for eve 
+ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 //  logger settings
-
-logger.add(new logger.transports.Console, {
+ logger.add(new logger.transports.Console, {
 colorize: true
 });
 logger.level = 'debug';
-
-var bot = new Discord.Client({
+ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
@@ -27,42 +19,29 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-
-    //change this to your key and pin
-    var key = 'kudo';
-    var pin = 'kudo';
-   
-   
-    if (message.substring(0, 1) == '!') {
+     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         var url = args[1];
-        
-
-
-        
-        
        
         args = args.splice(1);
-
-        if (cmd == 'quicktask') {
-            const link = 'http://everoboticslm.herokuapp.com/api/quick_task?key='+key+'&PIN='+pin+'&link='+url;
+        switch(cmd) {
+            // types of commands
+            case 'quicktask':
+                const link = 'http://127.0.0.1:5000/quicktask/'+ url;
                 // fetch(link)
                 // .then(data=>{return data.json()})
                 // .then(res=>{console.log(res)})     not using anymore
-
-                var xhr = new XMLHttpRequest();
+                 var xhr = new XMLHttpRequest();
                 xhr.open('GET', link, true);
                 xhr.send();
-
-
-
-                bot.sendMessage({
+                 bot.sendMessage({
                     to: channelID,
                     message: 'Quick task request successful! Check your bot!'
                 });
-            } else if (cmd == 'masslink') {
-                const mlink = 'http://everoboticslm.herokuapp.com/api/mass_edit?key='+key+'&PIN='+pin+'&link='+url;
+            break;
+            case 'masslink':
+                const mlink = 'http://127.0.0.1:5000/masslink/'+ url;
                     // fetch(link)
                     // .then(data=>{return data.json()})
                     // .then(res=>{console.log(res)})
@@ -73,43 +52,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Mass link change request successful! Check your bot!'
                 });
-            } else if (cmd == 'update') {
-                var key = args[0];
-                var pin = args[1];
-                
-
-
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'key= '+key+' pin= '+pin
-                });
-            } else if (cmd == 'show') {
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'key= '+key+' pin= '+pin
-                });
-            } 
-        // switch(cmd) {
-            
-        //     // types of commands
-        //     case 'quicktask':
-                
-        //     break;
-        //     case 'masslink':
-                
-        //     break; 
-        //     case 'update':
-                
-                
-        //     break; 
-        //     case 'show':
-                
-                
-
-                
-        //     break; 
-
-        //     //cyber, PD quick task and other functions to be added here
-        //  }
+            break; 
+             //cyber, PD quick task and other functions to be added here
+         }
      }
-});
+}); 
